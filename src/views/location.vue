cd182b4375b8eb8c45a4e5ffc480576c02b7760d
Eterna1 <eternal_century@outlook.com>
2024-06-17 16:39:47 +0800

    分配展销台

--- src/views/location.vue
+++ src/views/location.vue
@@ -1,15 +1,16 @@
 <template>
     <el-card class="box-card" id="card">
-    <div class="container">
-        <el-table :data="facData" id="facTable" :height="height" :row-class-name="tableRowClassName">
-            <el-table-column type="selection" width="55">
-            </el-table-column>
-            <el-table-column property="facName" label="名称" width="250" align="center">
-            </el-table-column>
-        </el-table>
 
-        <el-table :data="locationData" id="locationTable" :height="height" :row-class-name="tableRowClassName">
-            <el-table-column type="selection" width="50">
+    <el-dialog :title="title" :visible.sync="dialogFormVisible" width="70%">
+        <el-table :data="EmptylocationData" id="EmptylocationTable" :height="400" border>
+            <el-table-column width="100">
+                <template slot-scope="scope">
+                    <el-radio
+                        class="radio"
+                        :label="scope.row"
+                        v-model="currentLoc"
+                    >&emsp;&emsp;&emsp;</el-radio>
+                </template>
             </el-table-column>
             <el-table-column property="pubId" label="ID" width="250" align="center">
             </el-table-column>
@@ -17,10 +18,60 @@
             </el-table-column>
             <el-table-column property="pubLocation" label="位置" width="250" align="center">
             </el-table-column>
-            <el-table-column property="pubDesc" label="描述" width="400" align="center">
+            <el-table-column property="pubDesc" label="描述" align="center">
+            </el-table-column>
+        </el-table>
+        <el-row style="margin-top: 40px;">
+            <el-button type="primary" @click=addLocation>分配</el-button>
+            <el-button type="danger" @click="dialogFormVisible = false">取消</el-button>
+        </el-row>
+    </el-dialog>
+
+    <div class="container">
+        <el-table :data="facData" id="facTable" :height="height" border>
+            <el-table-column  width="55">
+
+                <template slot-scope="scope">
+                    <el-radio
+                        class="radio"
+                        :label="scope.row"
+                        v-model="currentFac"
+                    >&emsp;&emsp;&emsp;</el-radio>
+                </template>
+
+            </el-table-column>
+            <el-table-column property="facId" v-if="false">
+            </el-table-column>
+            <el-table-column property="facName" label="名称" align="center">
+            </el-table-column>
+        </el-table>
+
+        <el-table :data="locationData" id="locationTable" :height="height" border :row-class-name="tableRowClassName">
+            <el-table-column width="50">
+                <template slot-scope="scope">
+                    <el-radio
+                        class="radio"
+                        :label="scope.row"
+                        v-model="deleteLoc"
+                    >&emsp;&emsp;&emsp;</el-radio>
+                </template>
+            </el-table-column>
+            <el-table-column property="pubId" label="ID" width="250" align="center">
+            </el-table-column>
+            <el-table-column property="pubCode" label="编号" width="250" align="center">
+            </el-table-column>
+            <el-table-column property="pubLocation" label="位置" width="250" align="center">
+            </el-table-column>
+            <el-table-column property="pubDesc" label="描述" align="center">
+            </el-table-column>
+            <el-table-column property="facId" v-if="false">
             </el-table-column>
         </el-table>
     </div>
+    <el-row style="margin-top: 40px;">
+        <el-button type="primary" @click=handleAdd>分配展销位</el-button>
+        <el-button type="danger" @click=deleteLocation>删除展销位</el-button>
+    </el-row>
     </el-card>
 
 </template>
@@ -31,16 +82,15 @@ export default {
     name: "factory",
     data() {
         return {
+            currentFac: null,
+            currentLoc: null,
+            deleteLoc: null,
             facData: [],
-
-            //分页相关
-            total: 0,
             locationData: [],
-            //查
-            select: '',
-            input: '',
-
-
+            EmptylocationData: [],
+            multipleSelection: [],
+            dialogFormVisible: false,
+            title: '请选择展销位',
             height: '200px'
         }
     },
@@ -63,126 +113,77 @@ export default {
                 this.locationData = response.data.data;
             })
         },
-        //搜索相关
-        search() {
+        getEmptyLocList()
+        {
             this.$axios({
                 method: 'post',
-                url: 'http://localhost:9090/searchFactory',
-                data: {
-                    selection: this.select,
-                    inputString: this.input
-                }
+                url: 'http://localhost:9090/getEmptyLocation',
             }).then(response => {
-                this.AllData = response.data.data;
-                this.total = response.data.data.length;
-                this.getPageInfo();
+                this.EmptylocationData = response.data.data;
             })
         },
-
-        // 新增
-        // 表单重置初始化
-        reset() {
-            this.form = {
-                id: null,
-                facName: null,
-                facDesc: null,
-                facAddress: null,
-                facPhone: null,
-            },
-                this.imageUrl = ''
+        handleAdd(){
+            if (this.currentFac == null) {
+                this.$message({
+                    message: '请选择供应商',
+                    type: 'error'
+                })
+            }else{
+                this.dialogFormVisible = true;
+                this.getEmptyLocList();
+            }
         },
-        add() {
-            this.reset()
-            this.dialogFormVisible = true
-            this.title = "新增厂商数据"
-        },
-        handleSuccess(response, file) {
-            console.log(response.data)
-            this.imageUrl = response.data
-        },
-
-        edit(index, row) {
-            this.reset()
-            this.form = JSON.parse(JSON.stringify(row));
-            this.imageUrl = this.form.facLogo
-            this.dialogFormVisible = true
-            this.title = "修改厂商数据"
-        },
-
-        remove(index, row) {
-            this.reset()
-            this.form = JSON.parse(JSON.stringify(row));
+        addLocation()
+        {
             var param = {
-                facId: this.form.facId
+                pubId: this.currentLoc.pubId,
+                pubCode: this.currentLoc.pubCode,
+                pubLocation: this.currentLoc.pubLocation,
+                pubDesc: this.currentLoc.pubDesc,
+                facId: this.currentFac.facId
             }
             this.$axios({
-                method: 'post',
-                url: 'http://localhost:9090/removeFactory',
-                data: param
+            method: 'post',
+            url: 'http://localhost:9090/updateLocation',
+            data: param
             }).then(response => {
-                this.getList();
+                this.dialogFormVisible = false
+                this.getFacList()
+                this.getLocList()
                 this.$message({
                     message: response.data.message,
                     type: 'success'
                 });
             })
         },
-        handleSubmit() {
-            this.$refs['form'].validate((valid) => {
-                if (valid) {
-                    this.submit();
-                }
+        deleteLocation()
+        {
+            var param = {
+                pubId: this.deleteLoc.pubId,
+                pubCode: this.deleteLoc.pubCode,
+                pubLocation: this.deleteLoc.pubLocation,
+                pubDesc: this.deleteLoc.pubDesc,
+                facId: null
+            }
+            this.$axios({
+            method: 'post',
+            url: 'http://localhost:9090/updateLocation',
+            data: param
+            }).then(response => {
+                this.dialogFormVisible = false
+                this.getFacList()
+                this.getLocList()
+                this.$message({
+                    message: response.data.message,
+                    type: 'success'
+                });
             })
         },
-
-        //提交按钮
-        submit() {
-            if (this.form.facId == null) {
-                var param = {
-                    facId: '',
-                    facName: this.form.facName,
-                    facDesc: this.form.facDesc,
-                    facAddress: this.form.facAddress,
-                    facPhone: this.form.facPhone,
-                    facLogo: this.imageUrl,
-                    facState: this.radio
-                }
-                this.$axios({
-                    method: 'post',
-                    url: 'http://localhost:9090/addFactory',
-                    data: param
-                }).then(response => {
-                    this.getList();
-                    this.dialogFormVisible = false
-                    this.$message({
-                        message: response.data.message,
-                        type: 'success'
-                    });
-                })
-            } else {
-                var param = {
-                    facId: this.form.facId,
-                    facName: this.form.facName,
-                    facDesc: this.form.facDesc,
-                    facAddress: this.form.facAddress,
-                    facPhone: this.form.facPhone,
-                    facLogo: this.form.facLogo,
-                    facState: this.radio
-                }
-                this.$axios({
-                    method: 'post',
-                    url: 'http://localhost:9090/editFactory',
-                    data: param
-                }).then(response => {
-                    this.getList();
-                    this.dialogFormVisible = false
-                    this.$message({
-                        message: response.data.message,
-                        type: 'success'
-                    });
-                })
+        tableRowClassName({ row, rowIndex }) {
+            if (row.facId == null) {
+                return 'success-row';
             }
-
+            return '';
         },
         getHeight() {
             let num = document.getElementById("card").clientHeight - 150
@@ -208,12 +209,8 @@ export default {
     width: 130px;
 }
 
-.el-table .warning-row {
-    background: #FFEFD5;
-}
-
-.el-table .exit-row {
-    background: #FFC1C1;
+.el-table .success-row {
+    background: #f0f9eb;
 }
 
 .el-card {
